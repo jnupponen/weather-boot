05820befec795f9baab65a2d9b51614362b9b77d
jnupponen <jussi.nupponen@gmail.com>
2015-07-21 00:27:57 +0300

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,16 @@
+Weather Boot - Testing Spring Boot with Camel
+==============
+
+You will need to compile this example first:
+  mvn install
+
+To run the example type
+  mvn spring-boot:run
+
+You can also execute the fat WAR directly:
+
+  java -jar target/camel-example-spring-boot.war
+
+Camel will print current temperature at Tampere.
+
+To stop the example hit ctrl + c
---
+++ src/main/java/com/myrskytin/weatherboot/WeatherBootRoute.java
@@ -0,0 +1,44 @@
+package com.myrskytin.weatherboot;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.spring.boot.FatJarRouter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBootRoute extends FatJarRouter {
+
+	@Override
+	public void configure() {
+
+		from("netty-http://http://0.0.0.0:8181/saa").process(new Processor() {
+
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				exchange.getOut().setBody(null);
+
+			}
+		}).to("http4://ilmatieteenlaitos.fi/saa/tampere")
+				.convertBodyTo(String.class).process(new Processor() {
+
+					@Override
+					public void process(Exchange exchange) throws Exception {
+						Document doc = Jsoup.parse((String) exchange.getIn()
+								.getBody());
+						String temperature = doc
+								.select("tr.meteogram-temperatures").first()
+								.select("td").first().select("div").first()
+								.text().toString();
+
+						exchange.getOut().setBody(temperature);
+						exchange.getOut().setHeader(
+								"CamelHttpCharacterEncoding", "UTF-8");
+						exchange.getOut().setHeader("Content-Type",
+								"text/plain; charset=UTF-8");
+
+					}
+				}).to("log:temperature");
+	}
+}
